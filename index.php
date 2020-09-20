4d1e18bdca35592d168c760aad6821e1eb54cc84
GeorgeDzigo <mr.georgegamertv@gmail.com>
2020-09-20 16:42:20 +0300

    Added basic code to run web

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <title>SV - Home</title>
+      <!-- StyleSheets -->
+      <link rel="stylesheet" href="css/style.css">
+      <link rel="stylesheet" href="css/reset.css">
+      <!-- Favicon -->
+      <link rel="shortcut icon" href="img/favicon.png" type="image/x-icon">
+
+</head>
+<body>
+      
+      <!-- Header -->
+      <header class="header">
+            <div class="logo-holder">
+                  <img src="img/logo.png" alt="" id="log-holder-img">
+            </div>      
+      </header>
+      <!-- End Header -->
+</body>
+</html>
